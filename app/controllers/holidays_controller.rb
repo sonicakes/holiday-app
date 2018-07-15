439831e1dbca98aee78aaecad6cc4acad83c8cf6
sonicakes <soniaparker1207@gmail.com>
2018-07-15 11:27:08 +1000

    fixed bug with 'all holidays' link, added all CRUD for Holiday model

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -4,23 +4,39 @@ class HolidaysController < ApplicationController
   end
 
   def new
+      @holiday = Holiday.new
   end
 
   def show
       @holiday = Holiday.find params[:id]
   end
-  def create
-  end
 
   def edit
+      @holiday = Holiday.find params[:id]
   end
 
   def update
+    holiday = Holiday.find params[:id]
+holiday.update holiday_params
+redirect_to holiday
   end
 
   def delete
   end
 
-  def destroy
+  def create
+    holiday = Holiday.create holiday_params
+    redirect_to holiday # GET the show page
   end
+
+  def destroy
+    holiday = Holiday.find params[:id]
+    holiday.destroy
+    redirect_to holidays_path
+  end
+  # Strong params: create a whitelist of permitted parameters
+private
+def holiday_params
+  params.require(:holiday).permit(:title, :user_id)
+end
 end
