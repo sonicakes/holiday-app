64447f31fcf0b86ec4f4942e6d7feebccebdc031
Rowan Parker <rowanparker@gmail.com>
2018-07-19 19:35:41 +1000

    Fixed flash messages for login, logout, sign up and authentication errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
+      flash[:notice] = "Thanks for joining #{@user.name}"
       redirect_to root_path # Redirect to home if the account is valid
     else
       render :new # Let them retry the form again
