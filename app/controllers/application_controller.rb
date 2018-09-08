c6e9f272b31c50558f26e2096832f1b8ff4d96e4
sonicakes <soniaparker1207@gmail.com>
2018-09-08 18:14:36 +1000

    fixing heroku issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def current_user
+    # TODO if there is a session ID, but it doesn't match a user object
+    # delete the session id
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
